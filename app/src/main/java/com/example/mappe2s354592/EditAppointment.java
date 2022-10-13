fc95d52bb642eccdafe34109c0a927cfdc0952ba
Vilde <69961376+vildekure@users.noreply.github.com>
2022-10-13 16:24:48 +0200

    Lagd edit appoint funksjoner

--- app/src/main/java/com/example/mappe2s354592/EditAppointment.java
+++ app/src/main/java/com/example/mappe2s354592/EditAppointment.java
@@ -1,4 +1,71 @@
 package com.example.mappe2s354592;
 
-public class EditAppointment {
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+
+import androidx.annotation.Nullable;
+
+import com.example.mappe2s354592.Models.Appointment;
+import com.example.mappe2s354592.Models.Contact;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class EditAppointment extends Activity {
+    ImageButton backButton;
+    TextInputEditText innDate, innTime, innLoc, innMssg;
+    Button buttonEdit, buttonDelete;
+
+    Spinner spinnerContacts;
+    AdapterContact adapter;
+    DBHandler dbHelper;
+    SQLiteDatabase db;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editappointment);
+
+        dbHelper = new DBHandler(EditAppointment.this);
+        db = dbHelper.getWritableDatabase();
+
+        backButton = findViewById(R.id.button_back);
+        buttonEdit = findViewById(R.id.button_edit);
+        buttonDelete = findViewById(R.id.button_delete);
+
+        innDate = findViewById(R.id.date_text_field);
+        innTime = findViewById(R.id.time_text_field);
+        innLoc = findViewById(R.id.loc_text_field);
+        innMssg = findViewById(R.id.message_text_field);
+
+
+        spinnerContacts = findViewById(R.id.spinner_contacts);
+        spinnerContacts.setAdapter(adapter);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    public void editAppointments(View v) {
+        Appointment appointment = new Appointment();
+        appointment.setDate(innDate.getText().toString());
+        appointment.setTime(innTime.getText().toString());
+        appointment.setLocation(innLoc.getText().toString());
+        appointment.setMessage(innMssg.getText().toString());
+        dbHelper.editAppointment(db, appointment);
+    }
+
+    public void deleteContact (View v) {
+        Appointment appointment = new Appointment();
+        long appointId = appointment.get_ID();
+        dbHelper.deleteAppointment(db, appointId);
+    }
 }
