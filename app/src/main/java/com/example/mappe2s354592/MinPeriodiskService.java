8383c15414c113af76a13c58b029f9005ee98352
Vilde <69961376+vildekure@users.noreply.github.com>
2022-10-24 13:29:34 +0200

    Satt notifikasjon til 00 valgt time

--- app/src/main/java/com/example/mappe2s354592/MinPeriodiskService.java
+++ app/src/main/java/com/example/mappe2s354592/MinPeriodiskService.java
@@ -35,7 +35,7 @@ public class MinPeriodiskService extends Service {
 
         java.util.Calendar cal = Calendar.getInstance();
         cal.set(Calendar.HOUR_OF_DAY, timeInt);
-        cal.set(Calendar.MINUTE, 28);
+        cal.set(Calendar.MINUTE, 0);
 
         Intent i = new Intent(this, MinSendService.class);
         PendingIntent pintent = PendingIntent.getService(this, 0, i, 0);
